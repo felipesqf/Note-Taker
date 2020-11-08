6b2310a3965ee7bbed7f65a710b695c47cc1184e
^Felipe <felipesqf@gmail.com>
2020-11-08 15:13:36 +1100

    adding comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,40 +1,33 @@
-
-// let tableData = require("../db/db.json");
-
 const fs = require('fs');
-let rawTableData = fs.readFileSync('./db/db.json');
+let rawTableData = fs.readFileSync('./db/db.json'); //reading the db file
 let tableData = JSON.parse(rawTableData);
 
 
 module.exports = function(app) {
+    //route to display notes
     app.get("/api/notes", function(req, res) {
         return res.json(tableData);
       });
-
+    //route to create note
     app.post("/api/notes", function(req, res) {
-        if (tableData.length == 0){
+        if (tableData.length == 0){ //if db is empty id = 0
             req.body.id= 1;
         }else{
-        req.body.id = tableData[tableData.length - 1].id +1 ;
+        req.body.id = tableData[tableData.length - 1].id +1 ; //selecting the last id on db and adding 1
         }
         req.body.id.toString()
         tableData.push(req.body);
         let tableDataString = JSON.stringify(tableData)
-        fs.writeFileSync('./db/db.json', tableDataString)
+        fs.writeFileSync('./db/db.json', tableDataString) //writing new array into the db
         return res.json(tableData);
     });
-
+    //Route to delete note
     app.delete("/api/notes/:id", function(req, res) {
-        const toFilter = parseInt(req.params.id)
-        tableData = tableData.filter((item) => item.id !== toFilter)
+        const toFilter = parseInt(req.params.id) 
+        tableData = tableData.filter((item) => item.id !== toFilter) //filtering the everything that is not equal to id
         let newTableData = JSON.stringify(tableData)
-        fs.writeFileSync('./db/db.json', newTableData)
+        fs.writeFileSync('./db/db.json', newTableData) //writing new array into the db
         return res.json(tableData);
     });
 }
 
-
-// * The following API routes should be created:
-// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-// * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,15 @@
-
 var path = require("path");
 
-let tableData = require("../db/db.json");
 //html routing
-
 module.exports = function(app) {
-
-
+//route for the notes page
   app.get("/notes.html", function(req, res) {
     res.sendFile(path.join(process.cwd(), "/public/notes.html"));
   });
 
-
+//rout for the main page and everything else
   app.get("*", function(req, res) {
     res.sendFile(path.join(process.cwd(), "/public/index.html"));
   });
 };
 
-
-
-
-// * The following HTML routes should be created:
-
-//   * GET `/notes` - Should return the `notes.html` file.
-
-//   * GET `*` - Should return the `index.html` file
-
-// * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
